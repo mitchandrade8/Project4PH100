0115da752cf18d4e4d64fefd5336c52f057df5e5
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-23 14:42:43 -0600

    -- Started working with sample data for Create ML -- Seeing the weight of different relationships

--- Project4PH100/ContentView.swift
+++ Project4PH100/ContentView.swift
@@ -8,25 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var wakeUp = Date.now
-    
+   
     var body: some View {
-        VStack {
-            Text(Date.now, format: .dateTime.day().month().year())
-                .font(.title)
-            Text(Date.now, format: .dateTime.hour().minute())
-                .font(.title2)
-        }
+       Text("Master")
     }
-    
-    func trivialExample() {
-        let components = Calendar.current.dateComponents([.hour, .minute], from: Date.now)
-        let hour = components.hour ?? 0
-        let minute = components.minute ?? 0
-    }
+
     
 }
 
 #Preview {
     ContentView()
 }
+
