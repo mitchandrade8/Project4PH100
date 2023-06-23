741e2e17ac0d56d587104f042953de06d1f5528a
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-23 10:12:51 -0600

    Initial Commit

---
+++ Project4PH100/Project4PH100App.swift
@@ -0,0 +1,17 @@
+//
+//  Project4PH100App.swift
+//  Project4PH100
+//
+//  Created by Mitch Andrade on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct Project4PH100App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
