04ec6d691caa55b4c6be44cb5d28255cdf86ac7d
mitchandrade8 <54218379+mitchandrade8@users.noreply.github.com>
2023-06-23 13:54:01 -0600

    -- Started the Project, add my own personal version of how i would apply it to an app

---
+++ Project4PH100/MasteryStepper.swift
@@ -0,0 +1,18 @@
+//
+//  MasteryStepper.swift
+//  Project4PH100
+//
+//  Created by Mitch Andrade on 6/23/23.
+//
+
+import SwiftUI
+
+struct MasteryStepper: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MasteryStepper()
+}
